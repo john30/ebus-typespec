dac2360e13823e7f7800a3e8730d9dfdef145c11
John <ebusd@ebusd.eu>
2024-06-23 08:01:42 +0200

    prefer union value with new typespec, add test for it

--- test/decorators.test.ts
+++ test/decorators.test.ts
@@ -40,6 +40,20 @@ describe("decorators", () => {
       strictEqual(getZz(runner.program, Test), 0xfe);
     });
 
+    it("set broadcast zz on model via union", async () => {
+      const { Test } = (await runner.compile(
+        `@zz(targets.Broadcast) @test model Test {}`
+      )) as { Test: Model };
+      strictEqual(getZz(runner.program, Test), 0xfe);
+    });
+
+    it("set broadcast zz on model via alias", async () => {
+      const { Test } = (await runner.compile(
+        `@zz(BROADCAST) @test model Test {}`
+      )) as { Test: Model };
+      strictEqual(getZz(runner.program, Test), 0xfe);
+    });
+
     it("emit diagnostic with invalid value", async () => {
       const diagnostics = await runner.diagnose(
         `@zz(0xaa) @test model Test {}`
